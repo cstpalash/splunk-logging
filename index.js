386b26ee97d45c8a7cb995b6ff6d57ea253c0aef
Palash Roy <cstpalash@gmail.com>
2017-12-27 20:39:01 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+function logData(){
+	var today = new Date();
+	var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+	var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+	var dateTime = date+' '+time;
+	console.log(dateTime + " : splunk-logging");
+	setTimeout(function () {
+	  logData();
+	},5000);
+}
+
+logData();
